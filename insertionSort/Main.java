6aca97796b0ff9496eda040765d016b102cd1ec1
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-05-22 15:35:42 +0200

    insertion

--- insertionSort/Main.java
+++ insertionSort/Main.java
@@ -10,8 +10,11 @@ public class Main {
         { // Sort a[] into increasing order.
             int N = a.length;
             for (int i = 1; i < N; i++) { // Insert a[i] among a[i-1], a[i-2], a[i-3]....
-                for (int j = i; j > 0 && less(a[j], a[j - 1]); j--)
+                for (int j = i; j > 0 && !less(a[j-1], a[j]); j--){
+                    StdOut.print(j);
                     exch(a, j, j - 1);
+                }
+                StdOut.println();
             }
         }
         // See page 245 for less(), exch(), isSorted(), and main().
