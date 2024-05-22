05cd625c075fff4b705b50b74e95690c28bafd1b
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-05-22 16:31:17 +0200

    print

--- insertionSort/Main.java
+++ insertionSort/Main.java
@@ -9,12 +9,14 @@ public class Main {
         // insertion sort;
         { // Sort a[] into increasing order.
             int N = a.length;
-            for (int i = 1; i < N; i++) { // Insert a[i] among a[i-1], a[i-2], a[i-3]....
-                for (int j = i; j > 0 && !less(a[j-1], a[j]); j--){
-                    StdOut.print(j);
+            for (int i = 1; i < N; i++) { // Insert a[i] among a[i-1], a[i-2], a[i-3]...
+                for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                    //StdOut.print(j);
                     exch(a, j, j - 1);
+
+                    //show(a);
+                    //StdOut.println();
                 }
-                StdOut.println();
             }
         }
         // See page 245 for less(), exch(), isSorted(), and main().
--- shellSort/Main.java
+++ shellSort/Main.java
@@ -5,16 +5,28 @@ import StdOut.StdOut;
 
 public class Main {
     /* void sort() */
+
     public static void sort(Comparable[] a) { // Sort a[] into increasing order.
         int N = a.length;
         int h = 1;
-        while (h < N / 3)
+        while (h < N / 3){
             h = 3 * h + 1; // 1, 4, 13, 40, 121, 364, 1093, ...
+            //StdOut.print("while loop 1");
+            //StdOut.println();
+        }
+            //StdOut.println("h final value is "+h);
         while (h >= 1) { // h-sort the array.
+            //StdOut.print("while loop 2");
+            //StdOut.println("h value is "+h);
+
             for (int i = h; i < N; i++) { // Insert a[i] among a[i-h], a[i-2*h], a[i-3*h]... .
-                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h)
+                StdOut.println(i+" "+a[i]+" "+a[i-h]+" "+less(a[i], a[i-h]));
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h){
+                    show(a);
                     exch(a, j, j - h);
+                }
             }
+
             h = h / 3;
         }
     }
@@ -43,7 +55,7 @@ public class Main {
     /* void show() */
     public static void show(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
-            StdOut.print(a[i]+" ");
+            StdOut.print(a[i] + " ");
         }
         StdOut.println();
     }
