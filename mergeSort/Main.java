21bd9b111c542f1559633a00f406fca1210792a2
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-05-23 20:47:57 +0200

    mergeSort

--- mergeSort/Main.java
+++ mergeSort/Main.java
@@ -4,12 +4,10 @@ import In.In;
 import StdOut.StdOut;
 
 public class Main {
-    /* void sort() */
-    private static Comparable[] aux;
 
-    public static void sort(Comparable[] a, int lo, int mid, int hi)// merge
-    { // Merge a[lo..mid] with a[mid+1..hi].
-        aux = new Comparable[a.length]; // Allocate space just once.
+    /* ALGORITHM START */
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) { // Merge a[lo..mid] with a[mid+1..hi].
         int i = lo, j = mid + 1;
         for (int k = lo; k <= hi; k++) // Copy a[lo..hi] to aux[lo..hi].
             aux[k] = a[k];
@@ -23,6 +21,24 @@ public class Main {
             else
                 a[k] = aux[i++];
     }
+
+    private static Comparable[] aux; // auxiliary array for merges
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length]; // Allocate space just once.
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) { // Sort a[lo..hi].
+        if (hi <= lo)
+            return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid); // Sort left half.
+        sort(a, mid + 1, hi); // Sort right half.
+        merge(a, lo, mid, hi); // Merge results (code on page 271).
+    }
+
+    /* ALGORITHM END */
 
     /* boolean less() */
     public static boolean less(Comparable v, Comparable w) {
